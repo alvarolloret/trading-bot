43296329d8c8afc03dae290a36aa81e7ed539f28
Alvaro Lloret de Muller <alvaro_lloret@hotmail.com>
2017-12-23 12:19:26 +0100

    Adding to trading-bot-v0 internet checking

--- trading-bot-v0.py
+++ trading-bot-v0.py
@@ -1,10 +1,27 @@
 import time
 import sys
-
+	
+try:
+    import httplib
+except:
+    import http.client as httplib
+  
+  
+def have_internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+	conn.request("HEAD", "/")
+	conn.close()
+	print "Connection made"
+	return True
+    except:
+	print "Connection failed"
+	conn.close()
+	return False
 
 def main(argv):
 	period = 10
-	
+	have_internet()
 	while True:
 		print "period"
 		time.sleep(period)
